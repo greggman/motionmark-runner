4cdbc6698b266ce07e84fdb1e21c795b74144a11
Gregg Tavares <gman@chromium.org>
2022-08-26 13:58:03 -0700

    separate by GPU

--- format-results.js
+++ format-results.js
@@ -1,8 +1,6 @@
 
 const fs = require('fs');
 
-const data = JSON.parse(fs.readFileSync(process.argv[2], {encoding: 'utf-8'}));
-
 function getAPI(s) {
   if (s.includes("OpenGL")) {
     return "OpenGL"
@@ -27,61 +25,92 @@ function getGPU(s) {
   }
 }
 
-const apis = [''];
-const gpus = [''];
-const methods = [''];
-const forces = [''];
-const shareds = [''];
-const seps = [undefined];
-const table = [
-  apis,
-  gpus,
-  methods,
-  forces,
-  shareds,
-  seps,
-];
-
 const re = /(.*?)(\w+):([a-z_()-]+):(\w+)$/i;
-let firstRow = true;
-for (const [test, testData] of Object.entries(data)) {
-  const row = [test];
-  table.push(row);
-  for (const [name, results] of Object.entries(testData)) {
-    const m = re.exec(name);
-    if (!m) {
-      throw Error(`could not parse "${name}"`);
+
+function separateTestsByGPU(data) {
+  const gpus = {};
+  for (const [test, testData] of Object.entries(data)) {
+    for (const [name, results] of Object.entries(testData)) {
+      const m = re.exec(name);
+      if (!m) {
+        throw Error(`could not parse "${name}"`);
+      }
+      const [, vendor, method, force, shared] = m;
+      const gpu = getGPU(vendor);
+      const gpuTests = gpus[gpu] || [];
+      gpus[gpu] = gpuTests;
+      const tests = gpuTests[test] || {};
+      gpuTests[test] = tests;
+      tests[name] = results;
     }
-    const [, vendor, method, force, shared] = m;
-    const gpu = getGPU(vendor);
-    const api = getAPI(vendor);
-    if (firstRow) {
-      apis.push(api);
-      gpus.push(gpu);
-      methods.push(method);
-      forces.push(force);
-      shareds.push(shared);
-      seps.push(undefined);
-    }
-    row.push(results.score | 0);
   }
-  firstRow = false;
+  return gpus;
 }
 
-const columnWidths = [];
-for (const row of table) {
-  for (let i = 0; i < row.length; ++i) {
-    if (row[i] !== undefined) {
-      columnWidths[i] = Math.max(columnWidths[i] || 0, row[i].toString().length);
+function printTableForTests(gpu, data) {
+  const apis = [''];
+  const gpus = [''];
+  const methods = [''];
+  const forces = [''];
+  const shareds = [''];
+  const seps = [undefined];
+  const table = [
+    apis,
+    gpus,
+    methods,
+    forces,
+    shareds,
+    seps,
+  ];
+  // gather data into rows of elements
+  let firstRow = true;
+  for (const [test, testData] of Object.entries(data)) {
+    const row = [test];
+    table.push(row);
+    for (const [name, results] of Object.entries(testData)) {
+      const m = re.exec(name);
+      if (!m) {
+        throw Error(`could not parse "${name}"`);
+      }
+      const [, vendor, method, force, shared] = m;
+      const api = getAPI(vendor);
+      if (firstRow) {
+        apis.push(api);
+        gpus.push(gpu);
+        methods.push(method);
+        forces.push(force);
+        shareds.push(shared);
+        seps.push(undefined);
+      }
+      row.push(results.score | 0);
+    }
+    firstRow = false;
+  }
+
+  // find the width of each column
+  const columnWidths = [];
+  for (const row of table) {
+    for (let i = 0; i < row.length; ++i) {
+      if (row[i] !== undefined) {
+        columnWidths[i] = Math.max(columnWidths[i] || 0, row[i].toString().length);
+      }
     }
   }
+
+  // print the table (replace undefined with '-')
+  for (const row of table) {
+    const line = [];
+    for (let i = 0; i < row.length; ++i) {
+      const s = row[i] === undefined ? ''.padStart(columnWidths[i], '-') : row[i];
+      line.push(s.toString().padStart(columnWidths[i] + 1));
+    }
+    console.log(line.join(''));
+  }
+  console.log('\n')
 }
 
-for (const row of table) {
-  const line = [];
-  for (let i = 0; i < row.length; ++i) {
-    const s = row[i] === undefined ? ''.padStart(columnWidths[i], '-') : row[i];
-    line.push(s.toString().padStart(columnWidths[i] + 1));
-  }
-  console.log(line.join(''));
+const data = JSON.parse(fs.readFileSync(process.argv[2], {encoding: 'utf-8'}));
+const gpus = separateTestsByGPU(data);
+for (const [gpu, data] of Object.entries(gpus)) {
+  printTableForTests(gpu, data);
 }
